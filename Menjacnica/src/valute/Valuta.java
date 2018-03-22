4d005f04e5c4a97b12c43c590a7074d36feb53af
kristinaDek <kristina.dekic8@gmail.com>
2018-03-22 15:28:51 +0100

    Uvedene equals, hashCode i toString metode u klasu Valuta.

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -52,5 +52,46 @@ public class Valuta {
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
 	}
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", datum=" + datum + ", prodajni="
+				+ prodajni + ", kupovni=" + kupovni + ", srednji=" + srednji + "]";
+	}
+	
+	
 }
